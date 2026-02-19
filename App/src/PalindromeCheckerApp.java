02d4bd0a6ba38329762d725fb24cbf9649d401bf
Saimon-Shaji <ss0175@srmist.edu.in>
2026-02-19 11:16:02 +0530

    [Saimon] Added usecaseName

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System");
